61422d413f034d3c0dc61db8b521aa73f229879f
Mads Ohm Larsen <mads.ohm@gmail.com>
2015-05-18 15:16:35 +0200

    Removed vendor assets

--- app/assets/javascripts/dj_mon/dj_mon.js
+++ app/assets/javascripts/dj_mon/dj_mon.js
@@ -1,42 +1,34 @@
-//= require dj_mon/bootstrap_tooltip
-//= require dj_mon/bootstrap_tab
-//= require dj_mon/bootstrap_popover
-//= require dj_mon/bootstrap_modal
-//= require dj_mon/mustache
+window.bindModalClicks = function() {
+  $('a[data-toggle=modal]').on('click', function() {
+    var template = $($(this).attr('href')).html();
+    var output = Mustache.render(template, { content: $(this).data('content') });
+    $(output).appendTo($('body'));
+  });
+};
 
-$(function(){
-
-  $('a[data-toggle="tab"]').bind('shown', function(e) {
+$(function() {
+  $('a[data-toggle="tab"]').bind('shown.bs.tab', function(e) {
     var currentTab = e.target;
     var tabContent = $($(currentTab).attr('href'));
     var dataUrl = tabContent.data('url');
 
     $.getJSON(dataUrl).success(function(data){
       var template = $('#dj_reports_template').html();
-      if(data.length > 0)
+
+      if(data.length > 0) {
         var output = Mustache.render(template, data);
-      else
-        var output = "<div class='alert centered'>No Jobs</div>";
+      } else {
+        var output = "<div class='alert alert-warning'>No Jobs</div>";
+      }
+
       tabContent.html(output);
+
+      bindModalClicks();
     });
-
-  })
-
-  $('.nav.nav-tabs li.active a[data-toggle="tab"]').trigger('shown');
-
-  $('a[rel=popover]').live('mouseenter', function(){
-    $(this).popover('show');
   });
 
-  $('a[rel=modal]').live('click', function(){
-    var template = $($(this).attr('href')).html();
-    var output = Mustache.render(template, { content: $(this).data('content') });
-    $(output).appendTo($('body')).show();
-  });
-
-  $('[data-dismiss="modal"]').live('click', function(){
-    $('.modal').hide().remove();
-  });
+  $('.nav.nav-tabs li.active a[data-toggle="tab"]').trigger('shown.bs.tab');
+  bindModalClicks();
 
   (function refreshCount() {
     $.getJSON(dj_counts_dj_reports_path).success(function(data){
@@ -46,6 +38,4 @@ $(function(){
       setTimeout(refreshCount, 5000);
     });
   })();
-
-})
-
+});
